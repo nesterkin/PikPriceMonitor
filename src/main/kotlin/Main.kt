ad2454d34b429e86783c4a1b00883bdeaa936e3f
Nesterkin Alexander <nesterkin.as@yandex.ru>
2020-01-13 07:47:50 +0300

    init commit 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,12 @@ import com.google.gson.JsonObject
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
-import java.io.*
+import java.io.BufferedWriter
+import java.io.File
 import java.io.File.separatorChar
+import java.io.FileWriter
 import java.nio.file.Paths
+import java.util.*
 
 
 object Main {
@@ -13,55 +16,51 @@ object Main {
         "https://api.pik.ru/v2/flat?block_id=118&bulk_id=&layout_id=&price_from=&price_to=&floor_from=&floor_to=&area_from=&area_to=&rooms=&studio=&penthouse=&settlement=&settlement_from=&settlement_to=&isFurniture=&finish=&settlement_fact=&initial_payment=&monthly_payment=&order=price&price_million=0&index_by=statistics&images=1&metadata=1&layouts=1&type=1,2&page="
 
     private lateinit var client: OkHttpClient
+    private val date = Calendar.getInstance().time.toString()
     private val responseDir = Paths.get("").toAbsolutePath().toString() + separatorChar +
             "src" + separatorChar +
             "main" + separatorChar +
             "response" + separatorChar
-    private val prettyFlats = responseDir + "PrettyAllFlats.json"
+    private val prettyFlats = responseDir + "PrettyAllFlats " + date + ".json"
     private val gson = GsonBuilder().setPrettyPrinting().create()
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val flats = if (File(prettyFlats).exists()) {
-            println("Cache mode")
-            val reader = BufferedReader(FileReader(prettyFlats))
-            val rawJson = reader.readText()
-            gson.fromJson<FlatWrapper>(rawJson, FlatWrapper::class.java).flats
-        } else {
-            println("Internet mode")
-            makeHttp()
-            val flats = mutableListOf<Flat>()
-            var i = 1
+        makeHttp()
+        val flats = mutableListOf<Flat>()
+        var i = 1
 
-            File(responseDir).mkdirs()
+        File(responseDir).mkdirs()
 
-            do {
-                val url = baseUrl + i
-                val pikResponse = gson.fromJson<PikResponse>(makeResponse(url), PikResponse::class.java)
-                val prettyWriter = BufferedWriter(FileWriter(responseDir + "PrettyPage$i.json"))
-                prettyWriter.write(gson.toJson(pikResponse))
-                prettyWriter.close()
-                i++
-
-                flats.addAll(pikResponse.flats)
-            } while (pikResponse.flats.isNotEmpty())
-
-            val prettyWriter = BufferedWriter(FileWriter(prettyFlats))
-            val myCustomArray = gson.toJsonTree(flats).asJsonArray
-            val jsonObject = JsonObject().apply {
-                add("flats", myCustomArray)
-            }
-            prettyWriter.write(gson.toJson(jsonObject))
+        do {
+            val url = baseUrl + i
+            val pikResponse = gson.fromJson<PikResponse>(makeResponse(url), PikResponse::class.java)
+            val prettyWriter = BufferedWriter(FileWriter(responseDir + "PrettyPage$i " + date + ".json"))
+            prettyWriter.write(gson.toJson(pikResponse))
             prettyWriter.close()
+            i++
 
-            flats
+            flats.addAll(pikResponse.flats)
+        } while (pikResponse.flats.isNotEmpty())
+
+        val prettyWriter = BufferedWriter(FileWriter(prettyFlats))
+        val myCustomArray = gson.toJsonTree(flats).asJsonArray
+        val jsonObject = JsonObject().apply {
+            add("flats", myCustomArray)
         }
+        prettyWriter.write(gson.toJson(jsonObject))
+        prettyWriter.close()
 
         val outputFlats = mutableListOf<PrintableFlat>()
         flats
-//            .filter { it.status == "free" }
+            .filter { it.status == "free" }
+            .filter { it.floor > 5 }
+            .filter { it.bulk.name != "Корпус 27" }
+            .filter { it.bulk.name != "Корпус 38" }
+            .filter { it.bulk.name != "Корпус 39" }
+            .filter { it.bulk.name != "Корпус 40" }
 //            .filter { it.rooms == "2" }
-//            .filter { it.bulk.settlement_year == 2020 }
+//            .filter { it.bulk.settlement_year == 2021 }
             .forEach {
                 outputFlats.add(
                     PrintableFlat(
